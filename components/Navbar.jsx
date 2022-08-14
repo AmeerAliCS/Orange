28293c0a18b2245784765539a7726493a87b5695
AliAdel199 <msc180271@gmail.com>
2022-08-14 14:14:17 +0300

    edit 2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,163 +1,3 @@
-// // export default function Navbar() {
-// //     return (
-// //       <>
-// //       <h1>Navbar Page</h1>
-// //       </>
-// //     )
-// //   }
-
-// import * as React from "react";
-// import AppBar from "@mui/material/AppBar";
-// import Box from "@mui/material/Box";
-// import Divider from "@mui/material/Divider";
-// import Drawer from "@mui/material/Drawer";
-// import IconButton from "@mui/material/IconButton";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemText from "@mui/material/ListItemText";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
-// import Button from "@mui/material/Button";
-// import Link from "next/link";
-// // import { motion } from "framer-motion";
-
-// const drawerWidth = 240;
-// const navItems = ["من نحن", "مشاريعنا", "خدماتنا"];
-
-// interface Props {
-//   /**
-//    * Injected by the documentation to work in an iframe.
-//    * You won't need it on your project.
-//    */
-//   window?: () => Window;
-// }
-
-// export default function Navbar(props) {
-//   const { window } = props;
-//   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-//   const handleDrawerToggle = () => {
-//     setMobileOpen(!mobileOpen);
-//   };
-
-//   const drawer = (
-//     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-//       <Link href="/">
-//         <Box
-//           component="img"
-//           sx={{
-//             height: 60,
-//             width: 145,
-//             maxHeight: { xs: 70, md: 60 },
-//             maxWidth: { xs: 150, md: 145 },
-//           }}
-//           alt="Logo"
-//           src="orange.svg"
-//         />
-//       </Link>
-//       <Divider />
-//       <List>
-//         {navItems.map((item) => (
-//           <ListItem key={item} disablePadding>
-//             <ListItemButton sx={{ textAlign: "center" }}>
-//               <ListItemText primary={item} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Box>
-//   );
-// return (
-//   <Box sx={{ display: "flex" }}>
-//     <AppBar
-//       component="nav"
-// sx={{
-//   justifyContent: "space-between",
-//   bgcolor: "background.paper",
-//   color: "black",
-//   boxShadow: 3,
-// }}
-//     >
-//       <Toolbar>
-//         <IconButton
-//           color="inherit"
-//           aria-label="open drawer"
-//           edge="start"
-//           onClick={handleDrawerToggle}
-//           sx={{ mr: 2, display: { sm: "none" } }}
-//         >
-//           <MenuIcon />
-//         </IconButton>
-//         <Typography
-//           variant="h6"
-//           component="div"
-//           sx={{
-//             flexGrow: 1,
-//             display: { xs: "none", sm: "block" },
-//             color: "black",
-//             fontSize: "25px",
-//             mr: 2,
-//             ml: 2,
-//           }}
-//         >
-//           تواصل معنا
-//         </Typography>
-//         <Box
-//           sx={{
-//             mr: "30%",
-
-//             display: { xs: "flex", sm: "block" },
-//             color: "black",
-//           }}
-//         >
-//           {navItems.map((item) => (
-//             <Button
-//               key={item}
-//               sx={{ color: "black", fontSize: 17, mr: 2, ml: 2 }}
-//             >
-//               {item}
-//             </Button>
-//           ))}
-//         </Box>
-//         <Link href="/">
-//           <Box
-//             component="img"
-//             sx={{
-//               height: 60,
-//               width: 145,
-//               maxHeight: { xs: 70, md: 60 },
-//               maxWidth: { xs: 150, md: 145 },
-//             }}
-//             alt="Logo"
-//             src="orange.svg"
-//           />
-//         </Link>
-//       </Toolbar>
-//     </AppBar>
-//     <Box component="nav">
-//       <Drawer
-//         variant="temporary"
-//         open={mobileOpen}
-//         onClose={handleDrawerToggle}
-//         ModalProps={{
-//           keepMounted: true, // Better open performance on mobile.
-//         }}
-//         sx={{
-//           display: { xs: "block", sm: "none" },
-//           "& .MuiDrawer-paper": {
-//             boxSizing: "border-box",
-//             width: drawerWidth,
-//           },
-//         }}
-//       >
-//         {drawer}
-//       </Drawer>
-//       </Box>
-//     </Box>
-//   );
-// }
 import * as React from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
@@ -248,14 +88,21 @@ function DrawerAppBar(props) {
             sx={{
               mr: "30%",
 
-              display: { xs: "flex", sm: "block" },
+              display: { xs: "none", sm: "block" },
               color: "black",
             }}
           >
             {navItems.map((item) => (
               <Button
                 key={item}
-                sx={{ color: "black", fontSize: 17, mr: 2, ml: 2 }}
+                sx={{
+                  color: "black",
+                  fontSize: 17,
+                  mr: 2,
+                  ml: 2,
+                  xs: "none",
+                  md: "block",
+                }}
               >
                 {item}
               </Button>
@@ -265,9 +112,11 @@ function DrawerAppBar(props) {
             component="img"
             sx={{
               height: 60,
-              width: 145,
+              width: { md: 145, xs: 1 },
               maxHeight: { xs: 70, md: 60 },
-              maxWidth: { xs: 150, md: 145 },
+              maxWidth: { xs: 1, md: 145 },
+              display: { xs: "flex" },
+              alignItems: { xs: "center" },
             }}
             alt="Logo"
             src="orange.svg"
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import { Grid } from "@mui/material";
 import Hero from "../components/home/Hero";
 import Testimonial from "../components/home/Testimonial";
 
 export default function Home() {
   return (
-    <div>
+    <Grid container>
       <Hero />
       <Testimonial />
-    </div>
+    </Grid>
   );
 }
