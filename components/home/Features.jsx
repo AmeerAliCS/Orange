a2041ac0dd16cc52378816483c2615238edac671
AliAdel199 <msc180271@gmail.com>
2022-08-17 14:56:33 +0300

    edit

--- components/home/Features.jsx
+++ components/home/Features.jsx
@@ -1,27 +1,38 @@
 /* eslint-disable jsx-a11y/alt-text */
 import { Box, Container, Stack, Typography } from "@mui/material";
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Features() {
-    return (
-      <>
-      <Container sx={{textAlign: 'center'}}>
-        <Stack p={10} direction='row' justifyContent='space-around'>
-        <Box>
-        <Image src='/features_icon/percent_icon.svg' width='140' height='100' />
-        <Typography variant="h4">اسعار تنافسية</Typography>  
-        </Box>
-        <Box>
-        <Image src='/features_icon/security_icon.svg' width='150' height='100' />
-        <Typography variant="h4">الموثوقية</Typography>  
-        </Box>
-        <Box>
-        <Image src='/features_icon/speed_icon.svg' width='100' height='100' />
-        <Typography variant="h4">السرعة</Typography>  
-        </Box>
+  return (
+    <>
+      <Container sx={{ textAlign: "center" }}>
+        <Stack p={10} direction="row" justifyContent="space-around">
+          <Box>
+            <Image
+              src="/features_icon/percent_icon.svg"
+              width="140"
+              height="100"
+            />
+            <Typography variant="h4">اسعار تنافسية</Typography>
+          </Box>
+          <Box>
+            <Image
+              src="/features_icon/security_icon.svg"
+              width="150"
+              height="100"
+            />
+            <Typography variant="h4">الموثوقية</Typography>
+          </Box>
+          <Box>
+            <Image
+              src="/features_icon/speed_icon.svg"
+              width="100"
+              height="100"
+            />
+            <Typography variant="h4">السرعة</Typography>
+          </Box>
         </Stack>
       </Container>
-      </>
-    )
-  }
-  
+    </>
+  );
+}
