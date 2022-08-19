6ddf1a1075ad254b50f8e82f8bef2fdc1eb4a77e
AliAdel199 <msc180271@gmail.com>
2022-08-20 01:54:53 +0300

    edit aboutUs page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,160 +1,179 @@
-import {Avatar, Box, Container, Grid, Paper, Stack, Typography} from "@mui/material";
-import {styled} from "@mui/material/styles";
+import {
+  Avatar,
+  Box,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
 import Image from "next/image";
 import Link from "next/link";
 
-
-const MyTitle = styled("span")(({theme}) => {
-    return {
-        fontWeight: 900,
-        color: "white",
-        lineHeight: "1em",
-        fontSize: "28px",
-        [theme.breakpoints.only('sm')]: {
-            fontSize: '18px',
-            lineHeight: ".7em",
-            fontWeight: 800,
-
-        },
-        [theme.breakpoints.only('xs')]: {
-            fontSize: '12px',
-            lineHeight: ".6em",
-            fontWeight: 900,
-
-        },
-    }
+const MyTitle = styled("span")(({ theme }) => {
+  return {
+    fontWeight: 900,
+    color: "white",
+    lineHeight: "1em",
+    fontSize: "28px",
+    [theme.breakpoints.only("sm")]: {
+      fontSize: "18px",
+      lineHeight: ".7em",
+      fontWeight: 800,
+    },
+    [theme.breakpoints.only("xs")]: {
+      fontSize: "12px",
+      lineHeight: ".6em",
+      fontWeight: 900,
+    },
+  };
 });
 
-const StyledBox = styled(Box)(({ssx, theme}) => {
-    return ({
-        color: "white",
-        padding: "1px",
-        margin: "1px",
-        textAlign: "center",
-        fontFamily: "Montserrat",
-        letterSpacing: ".1rem",
-        lineHeight: "2em",
-        display: "block",
-        ...ssx,
-        fontSize: "20px",
-        [theme.breakpoints.only('sm')]: {
-            fontSize: '14px',
-            letterSpacing: "0.08rem",
-            lineHeight: "1.5em",
-        },
-        [theme.breakpoints.only('xs')]: {
-            fontSize: '10px',
-            letterSpacing: "0.08rem",
-            lineHeight: "1.5em",
-        }
-    })
+const StyledBox = styled(Box)(({ ssx, theme }) => {
+  return {
+    color: "white",
+    padding: "1px",
+    margin: "1px",
+    textAlign: "center",
+    fontFamily: "Montserrat",
+    letterSpacing: ".1rem",
+    lineHeight: "2em",
+    display: "block",
+    ...ssx,
+    fontSize: "20px",
+    [theme.breakpoints.only("sm")]: {
+      fontSize: "14px",
+      letterSpacing: "0.08rem",
+      lineHeight: "1.5em",
+    },
+    [theme.breakpoints.only("xs")]: {
+      fontSize: "10px",
+      letterSpacing: "0.08rem",
+      lineHeight: "1.5em",
+    },
+  };
 });
 
-const Row = styled(Box)(({ssx}) => {
-    return ({
-        alignItems: "center",
-        display: 'flex',
-        justifyContent: 'space-around',
-        ...ssx
-    });
+const Row = styled(Box)(({ ssx }) => {
+  return {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-around",
+    ...ssx,
+  };
 });
 
-
-const StyledImg = styled("img")(({ssx, theme}) => {
-    return {
-        ...ssx,
-        [theme.breakpoints.only('xs')]: {
-            width: "37px",
-            height: "40px",
-        }
-    }
+const StyledImg = styled("img")(({ ssx, theme }) => {
+  return {
+    ...ssx,
+    [theme.breakpoints.only("xs")]: {
+      width: "37px",
+      height: "40px",
+    },
+  };
 });
 export default function Footer() {
-    return (
-        <Box
-            component="div"
-            sx={{
-                bgcolor: "#3E424B",
-                height: {sm: "300px", md: "357px"},
-                mt: "5em",
-                mb: "0px",
-            }}>
-            <Stack
-                direction="row"
-                justifyContent="space-evenly"
-                // alignItems="center"
-                spacing={0}>
-                
-                <Box pt={8}>
+  return (
+    <Box
+      component="div"
+      sx={{
+        bgcolor: "#3E424B",
+        height: { sm: "300px", md: "357px" },
+        mt: "5em",
+        mb: "0px",
+      }}
+    >
+      <Stack
+        direction="row"
+        justifyContent="space-evenly"
+        // alignItems="center"
+        spacing={0}
+      >
+        <Box pt={8}>
+          <StyledBox>
+            <MyTitle>سياسة الخصوصية</MyTitle>
+            <Typography>الاحكام والشروط</Typography>
 
-                <StyledBox>
-                    <MyTitle>سياسة الخصوصية</MyTitle>
-                    <Typography>
-                        الاحكام والشروط
-                    </Typography>
-                    
-                    <MyTitle>للعمل معنا</MyTitle>
-                    <Typography>
-                    hr@orange.com
-                    </Typography>
-                    
-                </StyledBox>
-                </Box>
-
-                <Box pt={8}>
-                <StyledBox>
-                    <MyTitle>مركز المساعدة</MyTitle>
-                    <Typography>
-                    الدعم الفني
-                    </Typography>
-                    <Typography>
-                    الإبلاغ عن مشكلة
-                    </Typography>
-           
-                </StyledBox>
-                </Box>
-
-
-                <Box pt={8}>
-
-                <StyledBox>
-                    <MyTitle>حول اورنج</MyTitle>
-                    <Typography>
-                    الاسئلة الشائعة
-                    </Typography>
-                    <Typography>
-                    مشاريعنا
-                    </Typography>
-                </StyledBox>
-                </Box>
-
-                <Box pt={8}>
-
-                <StyledBox >
-                    <StyledImg height="46px"  sx={{
-                        width: {sm: "150px", xs: "130px", md: "186px"},
-                    }} alt="orange logo" src="/orange.svg"/>
-                    <br/>
-                    شركة اورنج العالمية<br/>
-                    شنو يعني اورنج؟
-                    <br/>
-
-                    <br/>
-                     <Row ssx={{justifyContent: 'space-evenly'}}>
-                       <Link href="https://google.com"><a><Image width="40px" height="60px" alt="instagram-icon" src="/images/instagram.svg"/></a></Link>
-                       <Link href="https://google.com"><a><Image width="40px" height="60px" alt="instagram-icon" src="/images/instagram.svg"/></a></Link>
-                       <Link href="https://google.com"><a><Image width="40px" height="60px" alt="instagram-icon" src="/images/instagram.svg"/></a></Link>
-                    </Row>
-
-                </StyledBox>
-                </Box>
-
-            </Stack>
-            <StyledBox ssx={{
-                fontSize: {xs: "1em", md: "1.5em"}
-            }}>
-                © Copyright 2022 Orange Platform
-            </StyledBox>
+            <MyTitle>للعمل معنا</MyTitle>
+            <Typography>hr@orange.com</Typography>
+          </StyledBox>
         </Box>
-    )
+
+        <Box pt={8}>
+          <StyledBox>
+            <MyTitle>مركز المساعدة</MyTitle>
+            <Typography>الدعم الفني</Typography>
+            <Typography>الإبلاغ عن مشكلة</Typography>
+          </StyledBox>
+        </Box>
+
+        <Box pt={8}>
+          <StyledBox>
+            <MyTitle>حول اورنج</MyTitle>
+            <Typography>الاسئلة الشائعة</Typography>
+            <Typography>مشاريعنا</Typography>
+          </StyledBox>
+        </Box>
+
+        <Box pt={8}>
+          <StyledBox>
+            <StyledImg
+              height="46px"
+              sx={{
+                width: { sm: "150px", xs: "130px", md: "186px" },
+              }}
+              alt="orange logo"
+              src="/orange.svg"
+            />
+            <br />
+            شركة اورنج العالمية
+            <br />
+            شنو يعني اورنج؟
+            <br />
+            <br />
+            <Row ssx={{ justifyContent: "space-evenly" }}>
+              <Link href="https://google.com">
+                <a>
+                  <Image
+                    width="40px"
+                    height="60px"
+                    alt="instagram-icon"
+                    src="/images/instagram.svg"
+                  />
+                </a>
+              </Link>
+              <Link href="https://google.com">
+                <a>
+                  <Image
+                    width="40px"
+                    height="60px"
+                    alt="instagram-icon"
+                    src="/images/instagram.svg"
+                  />
+                </a>
+              </Link>
+              <Link href="https://google.com">
+                <a>
+                  <Image
+                    width="40px"
+                    height="60px"
+                    alt="instagram-icon"
+                    src="/images/instagram.svg"
+                  />
+                </a>
+              </Link>
+            </Row>
+          </StyledBox>
+        </Box>
+      </Stack>
+      <StyledBox
+        ssx={{
+          fontSize: { xs: "1em", md: "1.5em" },
+        }}
+      >
+        © Copyright 2022 Orange Platform
+      </StyledBox>
+    </Box>
+  );
 }
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,36 @@
+import { Box, Button, Grid, Paper, Stack, Typography } from "@mui/material";
+
 export default function About() {
-    return (
-      <>
-      <h1>About Page</h1>
-      </>
-    )
-  }
-  
+  return (
+        <Box>
+    <Stack sx={{ml:10,mr:10, pt: 15 }} direction="row-reverse" justifyItems="center"  justifyContent="space-around">
+      <Box width="50%">
+        <Typography variant="h2" color="#FF7700" textAlign="center" sx={{pt:10,pb:5
+         }}>
+          من نحن
+        </Typography>
+        <Typography variant="body1" color="#000" sx={{pl:10,pr:10,pb:5,direction: 'rtl',
+         }} textAlign="justify" fontSize={25} >
+         منصة orange للحلول التقنية والبرمجية نقدم خدمات وانظمة اداريه في مختلف المجالات نتميز بكوننا نقدم خدمة عمل المشاريع الخاصه (نظام اداري خاص ، تطبيق موبايل اندرويد و IOS ,عمل موقع ويب خاص ...) باسرع وقت واقل تكلفه متواجدين 7/24 كل ما عليك هو اعطاء المعلومات اللازمة ونحن نتكفل في الباقي
+        </Typography>
+      </Box>
+      <Box component="img"  width={500}
+      src="/images/aboutUs1.svg"
+      />
+    </Stack>
+    <Stack sx={{ml:10,mr:10,pt:5 }} direction="row" justifyItems="center"  justifyContent="space-around">
+      <Box width="50%">
+      
+        <Typography variant="body1" color="#000" sx={{pl:10,pr:10,pb:5,direction: 'rtl',
+         }} textAlign="justify" fontSize={25} >
+نقدم ايضا خدمات التصميم حيث نقوم بتصميم الهويات البصرية مثل الشعار وتصاميم السوشيال ميديا وايضا تصميم البرشور وتصميم الكرنقدم ايضا خدمات التصميم حيث نقوم بتصميم الهويات البصرية مثل الشعار وتصاميم السوشيال ميديا وايضا تصميم البرشور وتصميم الكروت الشخصية وتصاميم UI|UX
+بالاضافة الى العديد من الخدمات الاخرى مثل ادارة الصفحات وادارة الاعلانات وتصميم كافة البوسترات الخاصه بعملك لتكن مميزا دوماوت الشخصية وتصاميم UI|UX
+بالاضافة الى العديد من الخدمات الاخرى مثل ادارة الصفحات وادارة الاعلانات وتصميم كافة البوسترات الخاصه بعملك لتكن مميزا دوما        </Typography>
+      </Box>
+      <Box component="img"  width={500}
+      src="/images/aboutUs2.svg"
+      />
+    </Stack>
+      </Box>
+  );
+}
