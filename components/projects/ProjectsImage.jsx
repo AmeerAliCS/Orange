288d2361e109c6ed1e5ae5560cac39dd2dcfaf4a
Ameer Ali <johncena10021@gmail.com>
2022-08-26 02:27:44 +0300

    some fix in image path

--- components/projects/ProjectsImage.jsx
+++ components/projects/ProjectsImage.jsx
@@ -3,7 +3,7 @@ export default function ProjectsImage() {
     return (
        <>
        <Hero
-           imgUrl="../../public/projects_icon/projects_hero.svg"
+           imgUrl="projects_icon/projects_hero.svg"
            text="تمتاز المشاريع المبرمجة من قبل منصة اورنج بأنها برمجيات عالية الجودة والسرعة وتمت برمجتها بأحدث التقنيات والمعايير العالمية
            وكذلك تمتاز التصاميم المقدمة من منصتنا بأنها تصاميم عصرية تواكب التطور "
            />
