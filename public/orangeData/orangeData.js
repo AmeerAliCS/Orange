588db921043f393977fc15293d941feaa000b8dd
AliAdel199 <msc180271@gmail.com>
2022-09-03 02:08:04 +0300

    edit avatars image

--- public/orangeData/orangeData.js
+++ public/orangeData/orangeData.js
@@ -1,232 +1,213 @@
 export const ORANGE_EMAIL = "info@orange.com";
 
 export const programmingServiceData = [
-    {
-        id: 0,
-        title: 'برمجة تطبيقات الدسكتوب',
-        imageUrl: '/programming_service_icon/desktop_icon.svg',
-        
-    },
-    {
-        id: 1,
-        title: 'برمجة تطبيقات الموبايل',
-        imageUrl: '/programming_service_icon/mobile_icon.svg',
-        
-    },
-    {
-        id: 2,
-        title: 'تصميم مواقع الكترونية',
-        imageUrl: '/programming_service_icon/website_icon.svg',
-        
-    },
-    {
-        id: 3,
-        title: 'UI/UX تصميم واجهات',
-        imageUrl: '/programming_service_icon/ui_ux_icon.svg',
-        
-    },
-    {
-        id: 4,
-        title: 'برمجة مشاريع التخرج',
-        imageUrl: '/programming_service_icon/graduation_icon.svg',
-        
-    },
-    {
-        id: 5,
-        title: 'برمجة مشاريع للشركات',
-        imageUrl: '/programming_service_icon/company_icon.svg',
-        
-    },  
+  {
+    id: 0,
+    title: "برمجة تطبيقات الدسكتوب",
+    imageUrl: "/programming_service_icon/desktop_icon.svg",
+  },
+  {
+    id: 1,
+    title: "برمجة تطبيقات الموبايل",
+    imageUrl: "/programming_service_icon/mobile_icon.svg",
+  },
+  {
+    id: 2,
+    title: "تصميم مواقع الكترونية",
+    imageUrl: "/programming_service_icon/website_icon.svg",
+  },
+  {
+    id: 3,
+    title: "UI/UX تصميم واجهات",
+    imageUrl: "/programming_service_icon/ui_ux_icon.svg",
+  },
+  {
+    id: 4,
+    title: "برمجة مشاريع التخرج",
+    imageUrl: "/programming_service_icon/graduation_icon.svg",
+  },
+  {
+    id: 5,
+    title: "برمجة مشاريع للشركات",
+    imageUrl: "/programming_service_icon/company_icon.svg",
+  },
 ];
 
 export const designServiceData = [
-    {
-        id: 0,
-        title: 'تصاميم سوشيال ميديا ',
-        imageUrl: '/design_service_icon/social_media_icon.svg',
-        
-    },
-    {
-        id: 1,
-        title: 'تصميم شعارات',
-        imageUrl: '/design_service_icon/logo_design_icon.svg',
-        
-    },
-    {
-        id: 2,
-        title: 'تصميم كروت',
-        imageUrl: '/design_service_icon/card_design_icon.svg',
-        
-    },
-    {
-        id: 3,
-        title: 'تصميم مجلة',
-        imageUrl: '/design_service_icon/magazine_design_icon.svg',
-        
-    },
-    {
-        id: 4,
-        title: 'تصميم بروشور',
-        imageUrl: '/design_service_icon/brochure_design_icon.svg',
-        
-    },
-    {
-        id: 5,
-        title: 'تصميم بوسترات',
-        imageUrl: '/design_service_icon/poster_design_icon.svg',
-        
-    },
+  {
+    id: 0,
+    title: "تصاميم سوشيال ميديا ",
+    imageUrl: "/design_service_icon/social_media_icon.svg",
+  },
+  {
+    id: 1,
+    title: "تصميم شعارات",
+    imageUrl: "/design_service_icon/logo_design_icon.svg",
+  },
+  {
+    id: 2,
+    title: "تصميم كروت",
+    imageUrl: "/design_service_icon/card_design_icon.svg",
+  },
+  {
+    id: 3,
+    title: "تصميم مجلة",
+    imageUrl: "/design_service_icon/magazine_design_icon.svg",
+  },
+  {
+    id: 4,
+    title: "تصميم بروشور",
+    imageUrl: "/design_service_icon/brochure_design_icon.svg",
+  },
+  {
+    id: 5,
+    title: "تصميم بوسترات",
+    imageUrl: "/design_service_icon/poster_design_icon.svg",
+  },
 ];
 
 export const ourProgrammingData = [
-    {
-        id: 0,
-        title: 'تطبيق السلامة والمساعدة',
-        imageUrl: '/projects_icon/safety_app.svg',
-        
-    },
-    {
-        id: 1,
-        title: 'تطبيق مراقبة مرضى كورونا',
-        imageUrl: '/projects_icon/covid_app.svg',
-        
-    },
-    {
-        id: 2,
-        title: 'تطبيق تسجيل الحضور',
-        imageUrl: '/projects_icon/qr_code.svg',
-        
-    },
-    {
-        id: 3,
-        title: 'موقع مزود خدمة انترنت',
-        imageUrl: '/projects_icon/satellite.svg',
-        
-    },
-    {
-        id: 4,
-        title: 'تطبيق الاقساط',
-        imageUrl: '/projects_icon/installments.svg',
-        
-    },
-    {
-        id: 5,
-        title: 'تطبيق تحويل الاموال',
-        imageUrl: '/projects_icon/money_app.svg',
-        
-    },  
+  {
+    id: 0,
+    title: "تطبيق السلامة والمساعدة",
+    imageUrl: "/projects_icon/safety_app.svg",
+  },
+  {
+    id: 1,
+    title: "تطبيق مراقبة مرضى كورونا",
+    imageUrl: "/projects_icon/covid_app.svg",
+  },
+  {
+    id: 2,
+    title: "تطبيق تسجيل الحضور",
+    imageUrl: "/projects_icon/qr_code.svg",
+  },
+  {
+    id: 3,
+    title: "موقع مزود خدمة انترنت",
+    imageUrl: "/projects_icon/satellite.svg",
+  },
+  {
+    id: 4,
+    title: "تطبيق الاقساط",
+    imageUrl: "/projects_icon/installments.svg",
+  },
+  {
+    id: 5,
+    title: "تطبيق تحويل الاموال",
+    imageUrl: "/projects_icon/money_app.svg",
+  },
 ];
 
 export const ourDesignData = [
-    {
-        id: 0,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/desktop_icon.svg',
-        
-    },
-    {
-        id: 1,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/mobile_icon.svg',
-        
-    },
-    {
-        id: 2,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/website_icon.svg',
-        
-    },
-    {
-        id: 3,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/ui_ux_icon.svg',
-        
-    },
-    {
-        id: 4,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/graduation_icon.svg',
-        
-    },
-    {
-        id: 5,
-        title: 'No Name',
-        imageUrl: '/programming_service_icon/company_icon.svg',
-        
-    },  
+  {
+    id: 0,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/desktop_icon.svg",
+  },
+  {
+    id: 1,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/mobile_icon.svg",
+  },
+  {
+    id: 2,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/website_icon.svg",
+  },
+  {
+    id: 3,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/ui_ux_icon.svg",
+  },
+  {
+    id: 4,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/graduation_icon.svg",
+  },
+  {
+    id: 5,
+    title: "No Name",
+    imageUrl: "/programming_service_icon/company_icon.svg",
+  },
 ];
 
 export const testimonialData = [
-    {
-        id: 0,
-        num: "+8",
-        title: "سنوات خبرة",
-        imageUrl: "/images/svgex.svg",
-    },
-    {
-        id: 1,
-        num: "+50",
-        title: "زبون من دول مختلفه",
-        imageUrl: "/images/person.svg",
-    },
-    {
-        id: 2,
-        num: "+120",
-        title: "مشروع",
-        imageUrl: "/images/svgcode.svg",
-    },
+  {
+    id: 0,
+    num: "+8",
+    title: "سنوات خبرة",
+    imageUrl: "/images/svgex.svg",
+  },
+  {
+    id: 1,
+    num: "+50",
+    title: "زبون من دول مختلفه",
+    imageUrl: "/images/person.svg",
+  },
+  {
+    id: 2,
+    num: "+120",
+    title: "مشروع",
+    imageUrl: "/images/svgcode.svg",
+  },
 ];
 
 export const customersReviewData = [
-    {
-        text: "خدمة ممتازة جداً انصح بها وبشدة شغل سريع وجميل والسعر مناسب كلش اتمنالكم التوفيق والتميز الدائم",
-        vector: "/images/blue-vector.svg",
-        userImage: "/images/ali.png",
-        color: "blue",
-    },
-    {
-        text: "افضل خدمة رأيتها من ناحية السرعة والسعر اتمنى لكم التوفيق والتطور الدائم وانصح الجميع بهذه المنصة",
-        vector: "/images/orange-vector.svg",
-        userImage: "/images/ali.png",
-        color: "orange",
-    },
-    {
-        text: "حولت فكرتي لمشروع حقيقي بسهولة جداً وية اورنج جنت متوقع لازم انتظر اشهر حتى اصنع مشروعي شكراً",
-        vector: "/images/dark-vector.svg",
-        userImage: "/images/ali.png",
-        color: "#424A60",
-    },
+  {
+    text: "خدمة ممتازة جداً انصح بها وبشدة شغل سريع وجميل والسعر مناسب كلش اتمنالكم التوفيق والتميز الدائم",
+    vector: "/images/blue-vector.svg",
+    userImage: "/images/img3.jpeg",
+    color: "blue",
+  },
+  {
+    text: "افضل خدمة رأيتها من ناحية السرعة والسعر اتمنى لكم التوفيق والتطور الدائم وانصح الجميع بهذه المنصة",
+    vector: "/images/orange-vector.svg",
+    userImage: "/images/img1.jpeg",
+    color: "orange",
+  },
+  {
+    text: "حولت فكرتي لمشروع حقيقي بسهولة جداً وية اورنج جنت متوقعه لازم انتظر اشهر حتى اصنع مشروعي شكراً",
+    vector: "/images/dark-vector.svg",
+    userImage: "/images/img2.jpeg",
+    color: "#424A60",
+  },
 ];
 
 export const faqsData = [
-    {
-        id: 0,
-        state:false,
-        question: 'ماهي منصة اورنج ؟',
-        answer: 'منصة orange للحلول التقنية والبرمجية نقدم خدمات وانظمة اداريه في مختلف المجالات نتميز بكوننا نقدم خدمة عمل المشاريع الخاصه (نظام اداري خاص ، تطبيق موبايل اندرويد و IOS ,عمل موقع ويب خاص ...) باسرع وقت واقل تكلفه متواجدين 7/24 كل ما عليك هو اعطاء المعلومات اللازمة ونحن نتكفل في الباقي',
-    },
-    {
-        id: 1,
-        state:false,
-        question: 'كيف يمكنني طلب خدمة ؟',
-        answer: 'يمكنك طلب خدمة من خلال مراسلتنا عبر الوسائل المتوفرة داخل الموقع مثل الايميل او الفورم او من خلال وسائل التواصل الاجتماعي',
-    },
-    {
-        id: 2,
-        state:false,
-        question: 'هل بإمكاني إلغاء الطلب ؟',
-        answer: 'يمكنك إلغاء الطلب في حال لم نقم بالبدء به وقبل دفع العربون وخلاف ذلك سيسقط حقك في المطالبة بالعربون المتفق عليه بين طالب الخدمة ومنصة اورنج',
-    },
-    {
-        id: 3,
-        state:false,
-        question: 'ماهو العربون ؟',
-        answer: 'هو مبلغ مالي يمثل 50% من من سعر المشروع يتم دفعه من قبل الزبون إلى منصة اورنج لضمان حق الطرفين وعدم الاخلال بالاتفاق',
-    },
-    {
-        id: 4,
-        state:false,
-        question: 'كيف تحمي Orange خصوصيتي وتحافظ على أمان معلوماتي؟',
-        answer: 'نعلم أن الأمان والخصوصية هما عاملان مهمان بالنسبة إليك مثلما هو الأمر بالنسبة إلينا أيضًا. ونضع ضمن أولوياتنا توفير أمان قوي وجعلك واثقًا من أمان معلوماتك وإمكانية وصولك إليها عند الحاجة.',
-    },
+  {
+    id: 0,
+    state: false,
+    question: "ماهي منصة اورنج ؟",
+    answer:
+      "منصة orange للحلول التقنية والبرمجية نقدم خدمات وانظمة اداريه في مختلف المجالات نتميز بكوننا نقدم خدمة عمل المشاريع الخاصه (نظام اداري خاص ، تطبيق موبايل اندرويد و IOS ,عمل موقع ويب خاص ...) باسرع وقت واقل تكلفه متواجدين 7/24 كل ما عليك هو اعطاء المعلومات اللازمة ونحن نتكفل في الباقي",
+  },
+  {
+    id: 1,
+    state: false,
+    question: "كيف يمكنني طلب خدمة ؟",
+    answer:
+      "يمكنك طلب خدمة من خلال مراسلتنا عبر الوسائل المتوفرة داخل الموقع مثل الايميل او الفورم او من خلال وسائل التواصل الاجتماعي",
+  },
+  {
+    id: 2,
+    state: false,
+    question: "هل بإمكاني إلغاء الطلب ؟",
+    answer:
+      "يمكنك إلغاء الطلب في حال لم نقم بالبدء به وقبل دفع العربون وخلاف ذلك سيسقط حقك في المطالبة بالعربون المتفق عليه بين طالب الخدمة ومنصة اورنج",
+  },
+  {
+    id: 3,
+    state: false,
+    question: "ماهو العربون ؟",
+    answer:
+      "هو مبلغ مالي يمثل 50% من من سعر المشروع يتم دفعه من قبل الزبون إلى منصة اورنج لضمان حق الطرفين وعدم الاخلال بالاتفاق",
+  },
+  {
+    id: 4,
+    state: false,
+    question: "كيف تحمي Orange خصوصيتي وتحافظ على أمان معلوماتي؟",
+    answer:
+      "نعلم أن الأمان والخصوصية هما عاملان مهمان بالنسبة إليك مثلما هو الأمر بالنسبة إلينا أيضًا. ونضع ضمن أولوياتنا توفير أمان قوي وجعلك واثقًا من أمان معلوماتك وإمكانية وصولك إليها عند الحاجة.",
+  },
 ];
