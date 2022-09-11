7bb1933e120aeb6032f6fa3305388008b9cdddad
AliAdel199 <msc180271@gmail.com>
2022-09-11 14:36:21 +0300

    rrr

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import MessengerCustomerChat from "react-messenger-customer-chat";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 function Layout(props) {
   return (
@@ -9,12 +10,40 @@ function Layout(props) {
       <Navbar />
       {/* <h1>Hello World !!!!!</h1> */}
       {props.children}
-
-      <MessengerCustomerChat
+      <MessengerChat
+        pageId="110388588223967"
+        language="sv_SE"
+        themeColor={"#000000"}
+        bottomSpacing={300}
+        loggedInGreeting="loggedInGreeting"
+        loggedOutGreeting="loggedOutGreeting"
+        greetingDialogDisplay={"show"}
+        debugMode={true}
+        onMessengerShow={() => {
+          console.log("onMessengerShow");
+        }}
+        onMessengerHide={() => {
+          console.log("onMessengerHide");
+        }}
+        onMessengerDialogShow={() => {
+          console.log("onMessengerDialogShow");
+        }}
+        onMessengerDialogHide={() => {
+          console.log("onMessengerDialogHide");
+        }}
+        onMessengerMounted={() => {
+          console.log("onMessengerMounted");
+        }}
+        onMessengerLoad={() => {
+          console.log("onMessengerLoad");
+        }}
+      />
+      ,
+      {/* <MessengerCustomerChat
         pageId="110388588223967"
         appId="464199215610503"
         // htmlRef="<REF_STRING>"
-      />
+      /> */}
       <Footer />
     </>
   );
