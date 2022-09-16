727c562b7127524c0aef36454ec3a93b0d91de72
Ameer Ali <johncena10021@gmail.com>
2022-09-16 19:05:25 +0300

    fix contacts ui

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,18 +1,13 @@
 import Head from "next/head";
 import Hero from "../components/home/Hero";
-import { Box, Button, IconButton, TextField, Typography } from "@mui/material";
-import { ORANGE_EMAIL } from "../public/orangeData/orangeData";
+import { Box, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 const ContactImg = "/images/contact-us.svg";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import EmailIcon from "@mui/icons-material/Email";
-
-const facbookUrl = "www.facebook.com/fb.oranjs";
-const telegramUrl = "https://t.me/tel_orange";
-const emailUrl = "info.orange.com";
-const instgramUrl = "www.instagram.com/i.oranjs";
+import Link from "next/link";
 
 export default function Contact() {
   return (
@@ -33,173 +28,50 @@ export default function Contact() {
       <Box
         sx={{
           fontSize: { xs: 22, sm: 30 },
-          direction: "rtl",
+          // direction: "rtl",
           mt: 6,
         }}
       >
         <Typography
           sx={{ color: "#3c3a54", ml: { xs: 5 }, mr: { xs: 5 } }}
           textAlign="center"
-          fontSize="inherit"
-        >
-          يمكنكم طلب خدمة من خلال مراسلتنا عبر الايميل
-        </Typography>
-        <Typography
-          sx={{ color: "#3c3a54", ml: { xs: 5 }, mr: { xs: 5 } }}
-          fontSize={{ xs: 16, sm: 22 }}
-          display="block"
-          textAlign="center"
-        >
-          <a href="mailto:info.oranjs@gmail.com">{emailUrl}</a>
-        </Typography>
-
-        <Typography
-          sx={{ color: "#3c3a54", ml: { xs: 5 }, mr: { xs: 5 } }}
-          textAlign="center"
           mt={5}
           fontSize="inherit"
         >
-          او من خلال مراسلتنا عبر مواقع التواصل الاجتماعي فيس بوك انستغرام.
+          يمكنكم التواصل معنا لطلب خدمة من خلال الوسائل التالية{" "}
         </Typography>
-        <Stack direction="column" width={1} alignItems="center" sx={{ mt: 5 }}>
-          <Stack direction="column" alignItems="flex-end">
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-end"
-              spacing={1}
-            >
-              <Typography
-                sx={{ color: "#3c3a54", ml: { xs: 2 }, mr: { xs: 5 } }}
-                fontSize={{ xs: 14, sm: 22 }}
-                textAlign="center"
-              >
-                <a href="mailto:info.oranjs@gmail.com">{emailUrl}</a>
-              </Typography>
-              <EmailIcon fontSize="large" htmlColor="#FC9338" />
-            </Stack>
-
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-end"
-              spacing={1}
-            >
-              <Typography
-                sx={{ color: "#3c3a54", ml: { xs: 2 }, mr: { xs: 5 } }}
-                fontSize={{ xs: 14, sm: 22 }}
-                textAlign="center"
-              >
-                <a href="https://www.facebook.com/fb.oranjs?_rdc=1&_rdr">
-                  {facbookUrl}
-                </a>
-              </Typography>
+        <Stack
+          direction="row"
+          width={1}
+          spacing={2}
+          justifyContent="center"
+          alignItems="center"
+          sx={{ mt: 5 }}
+        >
+          <Link href="https://www.facebook.com/fb.oranjs">
+            <a target="_blank">
               <FacebookIcon fontSize="large" htmlColor="#FC9338" />
-            </Stack>
+            </a>
+          </Link>
 
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-end"
-              spacing={1}
-            >
-              <Typography
-                sx={{ color: "#3c3a54", ml: { xs: 2 }, mr: { xs: 5 } }}
-                fontSize={{ xs: 14, sm: 22 }}
-                textAlign="center"
-              >
-                <a href="https://www.instagram.com/i.oranjs/">{instgramUrl}</a>
-              </Typography>
+          <Link href="https://www.instagram.com/i.oranjs">
+            <a target="_blank">
               <InstagramIcon fontSize="large" htmlColor="#FC9338" />
-            </Stack>
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="flex-end"
-              spacing={1}
-            >
-              <Typography
-                sx={{ color: "#3c3a54", ml: { xs: 2 }, mr: { xs: 5 } }}
-                fontSize={{ xs: 14, sm: 22 }}
-                textAlign="center"
-              >
-                <a href="https://t.me/tel_orange">{telegramUrl}</a>
-              </Typography>
+            </a>
+          </Link>
+
+          <Link href="https://t.me/tel_orange">
+            <a target="_blank">
               <TelegramIcon fontSize="large" htmlColor="#FC9338" />
-            </Stack>
-          </Stack>
+            </a>
+          </Link>
+
+          <Link href="mailto:info.oranjs@gmail.com">
+            <a target="_blank">
+              <EmailIcon fontSize="large" htmlColor="#FC9338" />
+            </a>
+          </Link>
         </Stack>
-        {/* <Typography
-          sx={{ color: "#3c3a54", ml: { xs: 5 }, mr: { xs: 5 } }}
-          textAlign="center"
-          mt={3}
-          fontSize="inherit"
-        >
-          كما يمكنكم طلب الخدمة من خلال الاستمارة التالية
-        </Typography>
-
-        <Box
-          dir="rtl"
-          sx={{
-            textAlign: "center",
-            pt: 5,
-            width: "70%",
-            margin: "auto",
-          }}
-        >
-          <div>
-            <TextField
-              placeholder="اسمك"
-              type="text"
-              sx={{
-                width: "35%",
-              }}
-            />
-            <TextField
-              placeholder="البريد الالكتروني"
-              type="email"
-              sx={{
-                width: "60%",
-                mr: "5%",
-              }}
-            />
-          </div>
-          <div>
-            <TextField
-              placeholder="نوع الخدمة التي تريدها"
-              type="text"
-              sx={{
-                pt: 3,
-                width: "100%",
-              }}
-            />
-          </div>
-
-          <div>
-            <TextField
-              placeholder="تفاصيل الخدمة"
-              multiline
-              rows={4}
-              fullWidth={true}
-              sx={{ pt: 3 }}
-            />
-          </div>
-
-          <div>
-            <Button
-              style={{
-                borderRadius: 10,
-                backgroundColor: "#FF7700",
-                fontSize: "25px",
-                width: "150px",
-                marginTop: "5vh",
-              }}
-              variant="contained"
-            >
-              ارسال
-            </Button>
-          </div>
-        </Box> */}
       </Box>
     </>
   );
