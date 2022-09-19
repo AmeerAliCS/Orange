f18ce49faae8109e307e482330726934c426886c
Ameer Ali <johncena10021@gmail.com>
2022-09-19 09:30:45 +0300

    fix programming heading size

--- components/home/ProgrammingService.jsx
+++ components/home/ProgrammingService.jsx
@@ -20,7 +20,7 @@ export default function ProgrammingService() {
           <Typography
             sx={{ color: "#3F3D56" }}
             mt={6}
-            variant="h5"
+            variant="h4"
             component="h1"
           >
             الخدمات البرمجية
