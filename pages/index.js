83513a33e3bdfac40d9fa6776b14a0312313e3cc
AliAdel199 <msc180271@gmail.com>
2022-09-20 03:40:40 +0300

    some edit

--- pages/index.js
+++ pages/index.js
@@ -7,26 +7,29 @@ import CustomersReview from "../components/home/CustomersReview";
 import Head from "next/head";
 const heroGif = "/images/heroImage.svg";
 
+const hrefUrl = "https://orange-hbiqsa047-aliadel199.vercel.app/";
 export default function Home() {
   return (
     <>
       <Head>
         <title>منصة اورنج لخدمات البرمجة والتصميم</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="title" content="منصة اورنج لخدمات البرمجة والتصميم" />
+        <meta name="og:title" content="منصة اورنج لخدمات البرمجة والتصميم" />
         <meta
-          name="description"
+          name="og:description"
           content="نقدم لكم خدمات مميزة لبرمجة تطبيقات الموبايل والمواقع الالكترونية وبرامج سطح المكتب ومشاريع التخرج ونقدم خدمة التصاميم العصرية بجميع انواعها"
         />
         <meta
           name="keywords"
           content="تطبيقات موبايل, تطبيقات سطح المكتب, مواقع الكترونية, انظمة ادارية, مشاريع تخرج, برامج مطاعم, برامج صيدلية. برامج سوبر ماركت, برامج مخازن, تصميم بوسترات, تصميم شعارات, تصميم هوية بصرية, تصميم فلكس, تصميم رولات, تصميم اعلانات, تصاميم سوشيال ميديا, منصة اورنج,"
         />
-        <meta name="robots" content="index, follow" />
+        {/* <meta name="robots" content="index, follow" /> */}
+        <meta name="robots" content="all" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="Arabic" />
         <meta name="author" content="orange platform" />
         <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="canonical" href={hrefUrl} />
       </Head>
 
       <Hero
