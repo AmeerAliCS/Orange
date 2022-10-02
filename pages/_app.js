2cb108c48cc947302b1af55f6f88c63fa72e2425
AliAdel199 <msc180271@gmail.com>
2022-10-02 12:52:50 +0300

    solve error

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,12 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Script
-        id="googleManager"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      /> */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
       <Script
         id="gtag-init"
-        // strategy="afterInteractive"
         strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
@@ -29,17 +23,7 @@ function MyApp({ Component, pageProps }) {
           `,
         }}
       />
-      {/* 
-      <Script id="googleLazy" strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script> */}
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
