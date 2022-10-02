19daec225b98d301f46e43f984fef413030c80f1
AliAdel199 <msc180271@gmail.com>
2022-10-02 12:32:19 +0300

    fix error

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-// import CustomersReviewCard from "../components/CustomersReviewCard";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="googleManager"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="googleLazy" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
